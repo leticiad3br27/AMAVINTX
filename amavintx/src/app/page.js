c3f08dee226e9f967ed78fd742f5b81a5f92567e
leticiad3br27 <leticiaSilva271107@gmail.com>
2025-01-27 14:28:45 -0400

    editeieditei

--- amavintx/src/app/page.js
+++ amavintx/src/app/page.js
@@ -7,7 +7,7 @@ function HomePage() {
     <div className="container">
       <h1>Bem-vindo!</h1>
       <p>Este é um exemplo de página utilizando o Bootstrap em um projeto Next.js.</p>
-      <Link href="/configurações">
+      <Link href="/configu">
         <button className="btn btn-primary">Ir para Configuração</button>
       </Link>
     </div>
